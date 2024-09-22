4deb37d6878f7804eb58ccd67b9bd3eecb5e537c
Biswa Mitra Barua <biswamitra960@gmail.com>
2024-09-22 11:58:01 +0600

    toggling and calculation and expense history on process

--- js/index.js
+++ js/index.js
@@ -21,11 +21,24 @@ calculateBtn.addEventListener('click', function () {
 
     const result = document.getElementById('results');
     result.classList.remove('hidden');
+
+    const historyItem = document.createElement('div');
+    historyItem.className = 'bg-white p-3 rounded-md border-l-2 border-indigo-500';
+
+    historyItem.innerHTML = `
+    <p class="text-xs text-gray-500">${new Date().toLocaleDateString()}</p>
+    <p>Income: ৳${income.toFixed(2)}</p>
+    <p>Expenses: ৳${totalExpense.toFixed(2)}</p>
+    <p>Balance: ৳${balance.toFixed(2)}</p>
+    `;
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild);
+
 })
 
 // savings calculation
 const calculateSavingsBtn = document.getElementById('calculate-savings');
-calculateSavingsBtn.addEventListener('click', function(){
+calculateSavingsBtn.addEventListener('click', function () {
     const savingPercentage = parseFloat(document.getElementById('savings').value);
     const income = parseFloat(document.getElementById('income').value);
     const software = parseFloat(document.getElementById('software').value);
@@ -41,4 +54,20 @@ calculateSavingsBtn.addEventListener('click', function(){
     const remainingBalance = document.getElementById('remaining-balance');
     const remainingAmount = balance - savingsAmount;
     remainingBalance.innerText = remainingAmount.toFixed(2);
+})
+
+// history tab functionality
+const historyTab = document.getElementById('history-tab');
+const assistantTab = document.getElementById('assistant-tab');
+historyTab.addEventListener('click', function () {
+    historyTab.classList.add(
+        'text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600'
+    );
+    historyTab.classList.remove('text-gray-600');
+    assistantTab.classList.remove(
+        'text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600'
+    );
+    assistantTab.classList.add('text-gray-600');
+
+    document.getElementById('expense-form').classList.add('hidden');
 })
